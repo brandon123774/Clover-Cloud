9efe689c486efa2573f2adedb8d428d0d6b6921f
Brandon Shankar <bshankar12@comcast.net>
2021-06-27 21:47:52 -0700

    working back end with some tests

--- edge-service/src/main/java/com/clover/edgeservice/exception/InvalidLogIdException.java
+++ edge-service/src/main/java/com/clover/edgeservice/exception/InvalidLogIdException.java
@@ -1,4 +1,7 @@
 package com.clover.edgeservice.exception;
 
-public class InvalidLogIdException {
+public class InvalidLogIdException extends RuntimeException {
+    public InvalidLogIdException(int id) {
+        super("No log exists with that id" + id);
+    }
 }
--- edge-service/src/main/java/com/clover/edgeservice/exception/InvalidProjectIdException.java
+++ edge-service/src/main/java/com/clover/edgeservice/exception/InvalidProjectIdException.java
@@ -1,4 +1,7 @@
 package com.clover.edgeservice.exception;
 
-public class InvalidProjectIdException {
+public class InvalidProjectIdException extends RuntimeException {
+    public InvalidProjectIdException(int id) {
+        super("No project exists with that id" + id);
+    }
 }
